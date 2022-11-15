2eaec06229934b0b8b22eefed60b4b57433f36e2
abygeorge8848 <70239768+abygeorge8848@users.noreply.github.com>
2022-11-15 18:08:51 +0530

    Add files via upload
    Prototype 3.0.2 comitted
    1. Patched the minus number and button bug during import of file. 
    2. Changed run file to import.
    3. Multiple files can be added on top of each other. 
    4. Steps can be added and removed from imported files

---
+++ Prototype 3.0 .2/src/Func/RunObj.java
@@ -0,0 +1,21 @@
+package Func;
+
+import java.util.ArrayList;
+
+public class RunObj implements java.io.Serializable{
+	
+ 	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String filename = "";
+	ArrayList<String> steps = new ArrayList<String>();
+	ArrayList<String> input1arr = new ArrayList<String>();
+	ArrayList<String> input2arr = new ArrayList<String>();
+	
+	public void main() {
+		
+	}
+	
+
+}
